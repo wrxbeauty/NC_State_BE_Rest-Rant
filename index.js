4cd0894305eb302bbb786f13be3927731435e0b1
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-02-22 16:33:45 -0500

    Adding 404 page view

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use('/places', require('./controllers/places.js'))
 
 app.get('/', (req, res) => {
+    // res.send('Hello World!')
     res.render('home')
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    // res.status(404).send('<h1>404 Page</h1>')
+    res.render('error404')
 })
 
 app.listen(process.env.PORT)
