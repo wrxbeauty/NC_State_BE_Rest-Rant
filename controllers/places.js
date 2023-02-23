439ab8079d53778d8ffe66e9b5a96704751edb00
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-02-22 22:13:56 -0500

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -12,7 +12,19 @@ router.get('/new', (req, res) => {
 
 router.post('/', (req, res) => {
     console.log(req.body)
-    res.send('POST /places')
-})
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+  })
+  
 
 module.exports = router
