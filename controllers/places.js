001a5d784a826719655d471ad1450dcc00b7e691
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-02-22 22:09:42 -0500

    Form data is getting through

--- controllers/places.js
+++ controllers/places.js
@@ -8,7 +8,11 @@ router.get('/', (req, res) => {
 
 router.get('/new', (req, res) => {
     res.render('places/new')
-  })
-  
+})
+
+router.post('/', (req, res) => {
+    console.log(req.body)
+    res.send('POST /places')
+})
 
 module.exports = router
