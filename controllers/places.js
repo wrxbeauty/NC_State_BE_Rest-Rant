7810f3af2553af5bd8297f6f121e6ec394bb7549
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-02-27 16:27:23 -0500

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -10,6 +10,19 @@ router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
+router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        res.render('places/show', {place: places[id]})
+    }
+  })
+  
 router.post('/', (req, res) => {
     console.log(req.body)
     if (!req.body.pic) {
@@ -24,7 +37,6 @@ router.post('/', (req, res) => {
     }
     places.push(req.body)
     res.redirect('/places')
-})
-
+}) 
 
 module.exports = router
