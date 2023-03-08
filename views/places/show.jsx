e9cb10fce348b983046a5969807d8f303ec3f304
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-03-08 18:31:01 -0500

    Populate comments

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,23 @@ const React = require('react')
 const Def = require('../default')
 
 function show({place, id}) {
+    let comments = (
+        <h3 className="inactive">No comments yet!</h3>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+          return (
+            <div className="border">
+              <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+              <h4>{c.content}</h4>
+              <h3>
+                <stong>- {c.author}</stong>
+              </h3>
+              <h4>Rating: {c.stars}</h4>
+            </div>
+          )
+        })
+      }
     return (
         <Def>
             <main>
@@ -38,7 +55,7 @@ function show({place, id}) {
                     </form>
                     <div>
                         <h2>Comments</h2>
-                        <p>No comments yet!</p>
+                        {comments}
                     </div>
                 </div>
             </main>
