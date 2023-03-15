13cc0d93e9cf8d90f371f97333489f23661b5f19
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-03-15 14:15:33 -0400

    Added stars emoji

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,7 +7,26 @@ function show({place, id}) {
             No comments yet!
         </h3>
     )
+    let rating = (
+        <h3>
+            Not yet rated
+        </h3>
+    )
+
     if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = Math.round(sumRatings / data.place.comments.length)
+        let stars = ''
+        for (let i = 0; i < averageRating; i++) {
+            stars += '⭐️'
+        }
+        rating = (
+            <h3>
+                {stars} stars
+            </h3>
+        )
         comments = data.place.comments.map(c => {
           return (
             <div className="border">
@@ -35,9 +54,8 @@ function show({place, id}) {
                         <h2>
                             Rating
                         </h2>
-                        <p>
-                            Not Rated
-                        </p>
+                        {rating}
+                        <br />
                     </div>
                     <div className="col-sm-6">
                         <h2>
