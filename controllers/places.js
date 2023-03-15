93c5808c649232db3370c0b7b75bfadc02d863cf
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-03-15 14:32:20 -0400

    Part 11

--- controllers/places.js
+++ controllers/places.js
@@ -1,60 +1,60 @@
 // Dependencies
 const router = require('express').Router()
-const db  = require('../models')
+const db = require('../models')
 const places = require('../models/places')
 
 // Index
 router.get('/', (req, res) => {
     db.Place.find()
-    .then((places) => {
-        res.render('places/index', { places })
-    })
-    .catch(err => {
-        console.log(err)
-        res.render('error404')
-    })
+        .then((places) => {
+            res.render('places/index', { places })
+        })
+        .catch(err => {
+            console.log(err)
+            res.render('error404')
+        })
 })
 
 // Create
 router.post('/', (req, res) => {
     db.Place.create(req.body)
-    .then(() => {
-        res.redirect('/places')
-    })
-    .catch(err => {
-        if (err && err.name == 'ValidationError') {
-            let message = 'Validation Error: '
-            for (var field in err.errors) {
-                message += `${field} was ${err.errors[field].value}`
-                message += `${err.errors[field].message}`
+        .then(() => {
+            res.redirect('/places')
+        })
+        .catch(err => {
+            if (err && err.name == 'ValidationError') {
+                let message = 'Validation Error: '
+                for (var field in err.errors) {
+                    message += `${field} was ${err.errors[field].value}`
+                    message += `${err.errors[field].message}`
+                }
+                res.render('places/new', { message })
             }
-            res.render('places/new', { message })
-        }
-        else {
-            res.render('error404')
-        }
-    })
+            else {
+                res.render('error404')
+            }
+        })
 })
 
 router.post('/:id/comment', (req, res) => {
     console.log(req.body)
     db.Place.findById(req.params.id)
-    .then(place => {
-        db.Comment.create(req.body)
-        .then(comment => {
-            place.comments.push(comment.id)
-            place.save()
-            .then(() => {
-                res.redirect(`/places/${req.params.id}`)
-            })
+        .then(place => {
+            db.Comment.create(req.body)
+                .then(comment => {
+                    place.comments.push(comment.id)
+                    place.save()
+                        .then(() => {
+                            res.redirect(`/places/${req.params.id}`)
+                        })
+                })
+                .catch(err => {
+                    res.render('error404')
+                })
         })
         .catch(err => {
             res.render('error404')
         })
-    })
-    .catch(err => {
-        res.render('error404')
-    })
 })
 
 // Create form
@@ -64,91 +64,68 @@ router.get('/new', (req, res) => {
 
 router.get('/:id', (req, res) => {
     db.Place.findById(req.params.id)
-    .populate('comments')
-    .then(place => {
-        console.log(place.comments)
-        res.render('places/show', { place })
-    })
-    .catch(err => {
-        console.log('err', err)
-        res.render('error404')
-    })
+        .populate('comments')
+        .then(place => {
+            console.log(place.comments)
+            res.render('places/show', { place })
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
 })
 
 router.post('/:id/comment', (req, res) => {
     console.log(req.body)
     db.Place.findById(req.params.id)
-    .then(place => {
-        db.Comment.create(req.body)
-        .then(comment => {
-            place.comments.push(comment.id)
-            place.save()
-            .then(() => {
-                res.redirect(`/places/${req.params.id}`)
-            })
+        .then(place => {
+            db.Comment.create(req.body)
+                .then(comment => {
+                    place.comments.push(comment.id)
+                    place.save()
+                        .then(() => {
+                            res.redirect(`/places/${req.params.id}`)
+                        })
+                })
+                .catch(err => {
+                    res.render('error404')
+                })
         })
         .catch(err => {
             res.render('error404')
         })
-    })
-    .catch(err => {
-        res.render('error404')
-    })
 })
 
 router.get('/:id/edit', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-        res.render('error404')
-    }
-    else if (!places[id]) {
-        res.render('error404')
-    }
-    else {
-      res.render('places/edit', { place: places[id] })
-    }
-  })
-  
+    db.Place.findById(req.params.id)
+        .then(place => {
+            res.render('places/edit', { place })
+        })
+        .catch(err => {
+            res.render('error404')
+        })
+})
 
-  router.put('/:id', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-        res.render('error404')
-    }
-    else if (!places[id]) {
-        res.render('error404')
-    }
-    else {
-        // Dig into req.body and make sure data is valid
-        if (!req.body.pic) {
-            // Default image if one is not provided
-            req.body.pic = 'http://placekitten.com/400/400'
-        }
-        if (!req.body.city) {
-            req.body.city = 'Anytown'
-        }
-        if (!req.body.state) {
-            req.body.state = 'USA'
-        }
-  
-        // Save the new data into places[id]
-        places[id] = req.body
-        res.redirect(`/places/${id}`)
-    }
-  })  
+router.put('/:id', (req, res) => {
+    db.Place.findByIdAndUpdate(req.params.id, req.body)
+        .then(() => {
+            res.redirect(`/places/${req.params.id}`)
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
+})
 
 router.delete('/:id', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-        res.render('error404')
-    }
-    else if (!places[id]) {
-        res.render('error404')
-    }
-    else {
-        places.splice(id, 1)
-        res.redirect('/places')
-    }
+    db.Place.findByIdAndDelete(req.params.id)
+        .then(place => {
+            res.redirect('/places')
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
 })
 
 module.exports = router
--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -10,45 +10,54 @@ function edit_form(data) {
                     <div className="row">
                         <div className="form-group col-sm-6">
                             <label htmlFor="name">Place Name:</label>
-                            <input 
-                                className="form-control" 
-                                id="name" 
-                                name="name" 
-                                value={data.place.name} 
-                            required />
+                            <input
+                                className="form-control"
+                                id="name"
+                                name="name"
+                                value={data.place.name}
+                                required />
                         </div>
                         <div className="form-group col-sm-6">
                             <label htmlFor="pic">Place Picture:</label>
-                            <input 
-                                className="form-control" 
-                                id="pic" 
+                            <input
+                                className="form-control"
+                                id="pic"
                                 name="pic" />
                         </div>
                         <div className="form-group col-sm-6">
+                            <label htmlFor="cuisines">Cuisines:</label>
+                            <input
+                                className="form-control"
+                                id="cuisines"
+                                name="cuisines" required />
+                        </div>
+                        <div className="form-group col-sm-6">
                             <label htmlFor="city">City:</label>
-                            <input 
-                                className="form-control" 
-                                id="city" 
+                            <input
+                                className="form-control"
+                                id="city"
                                 name="city" />
                         </div>
                         <div className="form-group col-sm-6">
                             <label htmlFor="state">State:</label>
-                            <input 
-                                className="form-control" 
-                                id="state" 
+                            <input
+                                className="form-control"
+                                id="state"
                                 name="state" />
                         </div>
                         <div className="form-group col-sm-6">
-                            <label htmlFor="cuisines">Cuisines:</label>
-                            <input 
-                                className="form-control" 
-                                id="cuisines" 
-                                name="cuisines" required />
+                            <label htmlFor="founded">Founded</label>
+                            <input
+                                className="form-control"
+                                id="founded"
+                                name="founded" 
+                                value= {data.place.founded}
+                                />
                         </div>
-                        <input 
-                            className="btn btn-primary" 
-                            type="submit" 
-                            value="Add Place" />
+                        <input
+                            className="btn btn-primary"
+                            type="submit"
+                            value="Update Place" />
                     </div>
                 </form>
             </main>
